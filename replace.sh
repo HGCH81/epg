aef50657706f37793012f97f23dec781d2bea935
hgoncalves <hgoncalves@ina.fr>
2020-01-10 17:52:47 +0100

    10012010

---
+++ replace.sh
@@ -0,0 +1,155 @@
+sed -i 's/RTP1/PT-RTP1/g' guide.xml
+sed -i 's/RTP2/PT-RTP2/g' guide.xml
+sed -i 's/RTP3/PT-RTP3/g' guide.xml
+sed -i 's/RTP Memória/PT-RTP_MEMORIA/g' guide.xml
+sed -i 's/RTP Africa/PT-RTP_AFRICA/g' guide.xml
+sed -i 's/RTP Internacional/PT-RTP_INTERNACIONAL/g' guide.xml
+sed -i 's/RTP Açores/PT-RTP_ACORES/g' guide.xml
+sed -i 's/RTP Madeira/PT-RTP_MADEIRA/g' guide.xml
+sed -i 's/SIC/PT-SIC/g' guide.xml
+sed -i 's/SIC Notícias/PT-SIC_NOTÍCIAS/g' guide.xml
+sed -i 's/SIC Radical/PT-SIC_RADICAL/g' guide.xml
+sed -i 's/SIC K/PT-SIC_K/g' guide.xml
+sed -i 's/SIC Mulher/PT-SIC_MULHER/g' guide.xml
+sed -i 's/SIC Caras/PT-SIC_CARAS/g' guide.xml
+sed -i 's/TVI/PT-TVI/g' guide.xml
+sed -i 's/TVI24/PT-TVI24/g' guide.xml
+sed -i 's/TVI Ficção/PT-TVI_FICCAO/g' guide.xml
+sed -i 's/CMTV/PT-CM_TV/g' guide.xml
+sed -i 's/Euronews/PT-EURONEWS/g' guide.xml
+sed -i 's/ARTV/PT-ARTV/g' guide.xml
+sed -i 's/Q/PT-Q/g' guide.xml
+sed -i 's/CANAL 180/PT-CANAL_180/g' guide.xml
+sed -i 's/Porto Canal/PT-PORTO_CANAL/g' guide.xml
+sed -i 's/Localvisão TV/PT-LOCALVISAO_TV/g' guide.xml
+sed -i 's/TPA Internacional/PT-TPA_INTERNACIONAL/g' guide.xml
+sed -i 's/TV Record/PT-TV_RECORD/g' guide.xml
+sed -i 's/Record News/PT-RECORD_NEWS/g' guide.xml
+sed -i 's/Cartoon Network/PT-CARTOON_NETWORK/g' guide.xml
+sed -i 's/Disney Channel/PT-DISNEY_CHANNEL/g' guide.xml
+sed -i 's/Disney Junior/PT-DISNEY_JUNIOR/g' guide.xml
+sed -i 's/Panda/PT-CANAL_PANDA/g' guide.xml
+sed -i 's/Biggs/PT-BIGGS/g' guide.xml
+sed -i 's/JimJam/PT-JIM_JAM/g' guide.xml
+sed -i 's/Baby TV/PT-BABY_TV/g' guide.xml
+sed -i 's/Eurosport/PT-EUROSPORT_1/g' guide.xml
+sed -i 's/Eurosport 2/PT-EUROSPORT_2/g' guide.xml
+sed -i 's/Eurosport News/PT-EUROSPORT_NEWS/g' guide.xml
+sed -i 's/SPORT.TV1/PT-SPORT_TV_1_HD/g' guide.xml
+sed -i 's/SPORT.TV2/PT-SPORT_TV_2_HD/g' guide.xml
+sed -i 's/SPORT.TV3/PT-SPORT_TV_3/g' guide.xml
+sed -i 's/SPORT.TV4/PT-SPORT_TV_4/g' guide.xml
+sed -i 's/SPORT.TV5/PT-SPORT_TV_5/g' guide.xml
+sed -i 's/BTV 1/PT-BTV_HD/g' guide.xml
+sed -i 's/Sporting TV/PT-SPORTING_TV/g' guide.xml
+sed -i 's/PFC/PT-PFC/g' guide.xml
+sed -i 's/Fuel TV/PT-FUEL_TV/g' guide.xml
+sed -i 's/A Bola TV/PT-A-BOLA-TV/g' guide.xml
+sed -i 's/Hollywood/PT-HOLLYWOOD/g' guide.xml
+sed -i 's/CINEMUNDO/PT-CINEMUNDO/g' guide.xml
+sed -i 's/AMC/PT-AMC/g' guide.xml
+sed -i 's/PT-TVCINE_1/PT-TVCINE_1/g' guide.xml
+sed -i 's/TVCine 2/PT-TVCINE_2/g' guide.xml
+sed -i 's/TVCine 3/PT-TVCINE_3/g' guide.xml
+sed -i 's/TVCine 4/PT-TVCINE_4/g' guide.xml
+sed -i 's/TVSéries/PT-TVS/g' guide.xml
+sed -i 's/FOX Movies/PT-FOX_MOVIES/g' guide.xml
+sed -i 's/FOX/PT-FOX/g' guide.xml
+sed -i 's/FOX Life/PT-FOX_LIFE/g' guide.xml
+sed -i 's/FOX Crime/PT-FOX_CRIME/g' guide.xml
+sed -i 's/FOX Comedy/PT-FOX_COMEDY/g' guide.xml
+sed -i 's/AXN/PT-AXN/g' guide.xml
+sed -i 's/AXN White/PT-AXN_WHITE_HD/g' guide.xml
+sed -i 's/AXN Black/PT-AXN_BLACK_HD/g' guide.xml
+sed -i 's/SyFy/PT-SYFY/g' guide.xml
+sed -i 's/MTV Portugal/PT-MTV_PORTUGAL/g' guide.xml
+sed -i 's/VH1/PT-VH1/g' guide.xml
+sed -i 's/C-Music/PT-C-MUSIC/g' guide.xml
+sed -i 's/Mezzo/PT-MEZZO/g' guide.xml
+sed -i 's/Afro Music/PT-AFRO_MUSIC/g' guide.xml
+sed -i 's/Clubbing TV/PT-CLUBBING_TV/g' guide.xml
+sed -i 's/Trace Urban/PT-TRACE_URBAN/g' guide.xml
+sed -i 's/MCM TOP/PT-MCM_TOP/g' guide.xml
+sed -i 's/MCM POP/PT-MCM_POP/g' guide.xml
+sed -i 's/Discovery Channel/PT-DISCOVERY_CHANNEL/g' guide.xml
+sed -i 's/National Geographic/PT-NAT_GEO/g' guide.xml
+sed -i 's/Nat Geo Wild/PT-NAT_GEO_WILD/g' guide.xml
+sed -i 's/Odisseia/PT-ODISSEIA/g' guide.xml
+sed -i 's/História/PT-HISTORIA/g' guide.xml
+sed -i 's/TLC/PT-TLC/g' guide.xml
+sed -i 's/Travel Channel/PT-TRAVEL_CHANNEL_HD/g' guide.xml
+sed -i 's/24Kitchen/PT-24_KITCHEN_HD/g' guide.xml
+sed -i 's/CBS Reality/PT-CBS_REALITY/g' guide.xml
+sed -i 's/Mas Chic/PT-MAS_CHIC/g' guide.xml
+sed -i 's/BBC Entertainment/PT-BBC_ENTERTAINMENT/g' guide.xml
+sed -i 's/TCV Internacional/PT-TCV_INTERNACIONAL/g' guide.xml
+sed -i 's/CNN/PT-CNN/g' guide.xml
+sed -i 's/Russia Today/PT-RUSSIA_TODAY/g' guide.xml
+sed -i 's/Aljazeera/PT-ALJAZEERA/g' guide.xml
+sed -i 's/BBC World/PT-BBC_WORLD/g' guide.xml
+sed -i 's/SKY News/PT-SKY_NEWS/g' guide.xml
+sed -i 's/i24 News (I)/PT-I24_NEWS_(I)/g' guide.xml
+sed -i 's/France 24 (I)/PT-FRANCE_24_(I)/g' guide.xml
+sed -i 's/France 24 (F)/PT-FRANCE_24_(F)/g' guide.xml
+sed -i 's/TV5 Monde/PT-TV5_MONDE/g' guide.xml
+sed -i 's/Bloomberg/PT-BLOOMBERG/g' guide.xml
+sed -i 's/FTV/PT-FTV/g' guide.xml
+sed -i 's/LUXE TV HD/PT-LUXE_TV_HD/g' guide.xml
+sed -i 's/Real Madrid TV/PT-REAL_MADRID_TV/g' guide.xml
+sed -i 's/TV Galícia/PT-TV_GALÍCIA/g' guide.xml
+sed -i 's/TVE 24H/PT-TVE_24H/g' guide.xml
+sed -i 's/KBS World/PT-KBS_WORLD/g' guide.xml
+sed -i 's/Playboy TV/PT-PLAYBOY_TV/g' guide.xml
+sed -i 's/HOT/PT-HOT/g' guide.xml
+sed -i 's/Venus/PT-VENUS/g' guide.xml
+sed -i 's/SexTreme/PT-SEXTREME/g' guide.xml
+sed -i 's/Penthouse HD1/PT-PENTHOUSE_HD1/g' guide.xml
+sed -i 's/Globo Premium/PT-GLOBO_PREMIUM/g' guide.xml
+sed -i 's/GLOBO/PT-GLOBO/g' guide.xml
+sed -i 's/TeleSUR/PT-TELESUR/g' guide.xml
+sed -i 's/TVIREAL/PT-TVI_REALITY/g' guide.xml
+sed -i 's/Nicke/PT-NICKELODEON/g' guide.xml
+sed -i 's/Nick Junior/PT-NICK_JUNIOR/g' guide.xml
+sed -i 's/AE/PT-A&E/g' guide.xml
+sed -i 's/Eleven Sports 1/PT-ELEVEN_SPORT_1_SD/g' guide.xml
+sed -i 's/Eleven Sports 2/PT-ELEVEN_SPORT_2_SD/g' guide.xml
+sed -i 's/Eleven Sports 3/PT-ELEVEN_SPORT_3/g' guide.xml
+sed -i 's/Eleven Sports 4/PT-ELEVEN_SPORT_4/g' guide.xml
+sed -i 's/Eleven Sports 5/PT-ELEVEN_SPORT_5/g' guide.xml
+sed -i 's/Eleven Sports 6/PT-ELEVEN_SPORT_6/g' guide.xml
+sed -i 's/CGTN/PT-CGTN/g' guide.xml
+sed -i 's/TRACE TOCA/PT-TRACE_TOCA/g' guide.xml
+sed -i 's/SPORT TV +/PT-SPORT_TV/g' guide.xml
+sed -i 's/KOMBAT SPORT/PT-KOMBAT_SPORT/g' guide.xml
+sed -i 's/ETB 1/PT-ETB_1/g' guide.xml
+sed -i 's/ETB 2/PT-ETB_2/g' guide.xml
+sed -i 's/ETB Sat/PT-ETB_SAT/g' guide.xml
+sed -i 's/LA 1/PT-LA_1/g' guide.xml
+sed -i 's/LA 2/PT-LA_2/g' guide.xml
+sed -i 's/Canal Extremadura/PT-CANAL_EXTREMADURA/g' guide.xml
+sed -i 's/CGTN Español/PT-CGTN_ESPAÑOL/g' guide.xml
+sed -i 's/RT Español/PT-RT_ESPAÑOL/g' guide.xml
+sed -i 's/RT Francia HD/PT-RT_FRANCIA_HD/g' guide.xml
+sed -i 's/Sky TG24 HD/PT-SKY_TG24_HD/g' guide.xml
+sed -i 's/RaiMovie/PT-RAIMOVIE/g' guide.xml
+sed -i 's/RaiPremium/PT-RAIPREMIUM/g' guide.xml
+sed -i 's/RaiNews24/PT-RAINEWS24/g' guide.xml
+sed -i 's/Deejay TV/PT-DEEJAY_TV/g' guide.xml
+sed -i 's/NRJ HITS/PT-NRJ_HITS/g' guide.xml
+sed -i 's/DELUXE MUSIC TV/PT-DELUXE_MUSIC_TV/g' guide.xml
+sed -i 's/Gong TV/PT-GONG_TV/g' guide.xml
+sed -i 's/Gong Max/PT-GONG_MAX/g' guide.xml
+sed -i 's/TV Escola [Brazil]/PT-TV_ESCOLA_[BRAZIL]/g' guide.xml
+sed -i 's/Futura [Brazil]/PT-FUTURA_[BRAZIL]/g' guide.xml
+sed -i 's/Rede TV [Brazil]/PT-REDE_TV_[BRAZIL]/g' guide.xml
+sed -i 's/SBT [Brazil]/PT-SBT_[BRAZIL]/g' guide.xml
+sed -i 's/ARD/PT-ARD/g' guide.xml
+sed -i 's/DW Deutsch/PT-DW_DEUTSCH/g' guide.xml
+sed -i 's/DW Deutsch Plus/PT-DW_DEUTSCH_PLUS/g' guide.xml
+sed -i 's/DW English/PT-DW_ENGLISH/g' guide.xml
+sed -i 's/DW Espanol/PT-DW_ESPANOL/g' guide.xml
+sed -i 's/Grand Lille TV/PT-GRAND_LILLE_TV/g' guide.xml
+sed -i 's/Alsace 20/PT-ALSACE_20/g' guide.xml
+sed -i 's/HispanTV/PT-HISPANTV/g' guide.xml
+sed -i 's/Motorvision TV/PT-MOTORVISION/g' guide.xml
+
